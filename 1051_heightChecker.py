1e378cc5e110ec523c8409b6d47f4d18afb512dd
Midhun <32930343+midhun98@users.noreply.github.com>
2022-12-06 16:19:38 +0530

    Create 1051_heightChecker.py
    https://www.midhun98.in/blog-list/28/

---
+++ 1051_heightChecker.py
@@ -0,0 +1,9 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        count = 0
+        sorted_height = sorted(heights)
+
+        for i in range(len(heights)):
+            if heights[i] != sorted_height[i]:
+                count += 1
+        return count
